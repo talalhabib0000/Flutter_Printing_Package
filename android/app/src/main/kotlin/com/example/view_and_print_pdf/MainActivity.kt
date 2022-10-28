189f028088885e583b88d6517036a99a9273ce67
Talal <talalhabib0000@outlook.com>
2022-10-28 15:28:43 +0500

    Flutter view and print

---
+++ android/app/src/main/kotlin/com/example/view_and_print_pdf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.view_and_print_pdf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Printing"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(minimumSize: const Size(200, 40)),
+              onPressed: _createPdf,
+              child: const Text(
+                'Create & Print PDF',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(minimumSize: const Size(200, 40)),
+              onPressed: _displayPdf,
+              child: const Text(
+                'Display PDF',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(minimumSize: const Size(200, 40)),
+              onPressed: generatePdf,
+              child: const Text(
+                'Generate Advanced PDF',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _createPdf() async {
+    final doc = pw.Document();
+
+    doc.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Text('Flutter Printing Demo'),
+          );
+        },
+      ),
+    );
+
+    await Printing.layoutPdf(
+        onLayout: (PdfPageFormat format) async => doc.save());
+  }
+
+  void _displayPdf() {
+    final doc = pw.Document();
+    doc.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Text(
+              'This is an Example of a way to display pdf',
+              style: const pw.TextStyle(fontSize: 30),
+            ),
+          );
+        },
+      ),
+    );
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PreviewScreen(doc: doc),
+        ));
+  }
+
+  void _convertPdfToImages(pw.Document doc) async {
+    await for (var page
+        in Printing.raster(await doc.save(), pages: [0, 1], dpi: 72)) {
+      final image = page.toImage();
+      print(image);
+    }
+  }
+
+  void _printExistingPdf() async {
+    final pdf = await rootBundle.load('assets/document.pdf');
+    await Printing.layoutPdf(onLayout: (_) => pdf.buffer.asUint8List());
+  }
+
+  Future<Uint8List> _generatePdf(PdfPageFormat format, String title) async {
+    final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: true);
+    final font = await PdfGoogleFonts.nunitoExtraLight();
+
+    pdf.addPage(
+      pw.Page(
+        pageFormat: format,
+        build: (context) {
+          return pw.Column(
+            children: [
+              pw.SizedBox(
+                width: double.infinity,
+                child: pw.FittedBox(
+                  child: pw.Text(title, style: pw.TextStyle(font: font)),
+                ),
+              ),
+              pw.SizedBox(height: 20),
+              pw.Flexible(child: pw.FlutterLogo())
+            ],
+          );
+        },
+      ),
+    );
+    return pdf.save();
+  }
+
+  void generatePdf() async {
+    const title = 'Printing Demo';
+    await Printing.layoutPdf(onLayout: (format) => _generatePdf(format, title));
+  }
+}
+
+class PreviewScreen extends StatelessWidget {
+  final pw.Document doc;
+
+  const PreviewScreen({
+    Key? key,
+    required this.doc,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back_outlined),
+        ),
+        centerTitle: true,
+        title: const Text("Preview"),
+      ),
+      body: PdfPreview(
+        build: (format) => doc.save(),
+        allowSharing: true,
+        allowPrinting: true,
+        initialPageFormat: PdfPageFormat.a4,
+        pdfFileName: "mydoc.pdf",
+      ),
+    );
+  }
+}
